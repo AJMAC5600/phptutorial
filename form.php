b18814dcd41e86cb31bbc839d3016a58fa2c739c
AJMAC5600 <56782083+AJMAC5600@users.noreply.github.com>
2024-12-02 18:05:38 +0530

    commit

--- form.php
+++ form.php
@@ -1,30 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-</head>
+    <link rel="stylesheet" href="style.css" />
+  </head>
 
-<body>
+  <body>
     <center>
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']?>">
+      <form method="POST" action="<?php echo $_SERVER['PHP_SELF']?>">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" />
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <input type="submit" value="Submit" name="submit" />
+      </form>
 
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-            <input type="submit" value="Submit" name="submit">
-        </form>
-
-        <div class="feedback">
-            <?php 
+      <div class="feedback">
+        <?php 
             if(isset($_POST['submit'])){
                 if($_SERVER['REQUEST_METHOD']==='POST'){
                     $username=htmlspecialchars($_POST['username']);
                     $pass=htmlspecialchars($_POST['password']);
-                   if(strcmp($username,"Omkar")===0 && strcasecmp($pass,"Omkar")===0){
+                   if(strcmp($username,"Aadarsh")===0 && strcasecmp($pass,"Aadarsh")===0){
                     header("location:fuck.html");
                     exit;
                    }else{
@@ -33,8 +32,7 @@
                 }
             }
             ?>
-        </div>
+      </div>
     </center>
-</body>
-
+  </body>
 </html>
