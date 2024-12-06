1c478fba2d9f47c66a7000d3b656c72d737dc4c3
Abhishek Jha <56782083+AJMAC5600@users.noreply.github.com>
2024-12-06 21:03:47 +0530

    commit

--- database.php
+++ database.php
@@ -1,16 +1,31 @@
 <?php 
-        if(isset($_POST['submit'])){
-            if($_SERVER['REQUEST_METHOD']==='POST'){
-                $mydata=Mysqli_connect("localhost","root","","emp");
-        if($mydata) echo "Connection is done";
-        $stmt=$mydata->prepare("INSERT INTO emp_mang VALUES(?,?,?,?)");
-        $stmt->bind_param("issi",$id,$name,$address,$number);
-        $id=htmlspecialchars($_POST['id']);
-        $name=htmlspecialchars($_POST['name']);
-        $address=htmlspecialchars($_POST['address']);
-        $number=htmlspecialchars($_POST['number']);
-        $stmt->execute();
-        echo  $id." ".$name." ".$address." ".$number;
-            }
-        }
-        ?>
+    $servername = "localhost";
+    $username="root";
+    $password="";
+    $dbname="emp";
+    $conn=new mysqli($servername,$username,$password,$dbname);
+    if($conn->connect_error) echo "the connection got this error ".$conn->connect_error;
+    else echo "Connected successfully";
+    if(isset($_POST['submit'])){
+        if($_SERVER['REQUEST_METHOD']==='POST'){
+           
+    $id=htmlspecialchars($_POST['id']);
+    $name=htmlspecialchars($_POST['name']);
+    $address=htmlspecialchars($_POST['address']);
+    $number=htmlspecialchars($_POST['number']);
+    $sql="INSERT INTO EMP_TABLE (id,name,address,phone) VALUES(?,?,?,?)";
+    $stmt=$conn->prepare($sql);
+    $stmt->bind_param("issi",$id,$name,$address,$number);
+    // $stmt->execute();
+    if($conn->affected_rows>0) echo "<br>the data is entered successfully";
+    $sql="select * from emp_table";
+    $output=$conn->query($sql);
+    echo "<pre>";
+    // print_r($output);
+    // while($row=$output->fetch_assoc()){
+    //     echo "<pre>";
+    //     print_r($row);
+    // }
+    print_r($output->fetch_assoc());
+        }}
+?>
