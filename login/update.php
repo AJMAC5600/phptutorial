e009dfc0b3d8c12c7a3da4ba79c844a70721a7d3
Abhishek Jha <56782083+AJMAC5600@users.noreply.github.com>
2024-12-10 07:23:37 +0530

    commit

--- login/update.php
+++ login/update.php
@@ -2,7 +2,6 @@
 include('config/dbconn.php');
 session_start();
 $id=$_POST['id'];
-
 $_SESSION['id']=$id;
 $sql="SELECT * FROM `user` WHERE `id`='$id'";
 $res1=$conn->query($sql);
@@ -10,21 +9,63 @@ $row1=$res1->fetch_assoc();
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="en" style="background-color: #333;">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
-<body>
-    <form action="update1.php" method='POST'>
+<body style="color: white;">
+    <form action="update1.php" method='POST' style="
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    ">
 
-        <input type="text" name="username" placeholder="New Username" value="<?php echo $row1['username'];?>" required>
-        <input type="text" name="password" placeholder="New Password" value="<?php echo $row1['password'];?>" required>
+        <input type="text" name="username" placeholder="New Username" value="<?php echo $row1['username'];?>" required style="
+        margin: 10px auto;
+        width: 300px;
+        height: 40px;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 18px;
+        box-sizing: border-box;
+        ">
+        <input type="text" name="password" placeholder="New Password" value="<?php echo $row1['password'];?>" required style="
+        margin: 10px auto;
+        width: 300px;
+        height: 40px;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 18px;
+        box-sizing: border-box;
+        ">
         <input type="hidden" name="id" value="<?php echo $row1['id'];?>">
-        <button type="submit" name="submit">Update</button>
+        <input type="date" name="date" value="<?php echo $row1['date'];?>" required style="
+        margin: 10px auto;
+        width: 300px;
+        height: 40px;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 18px;
+        box-sizing: border-box;
+        ">
+        <button type="submit" name="submit" style="
+        margin: 10px auto;
+        width: 300px;
+        height: 40px;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 18px;
+        box-sizing: border-box;
+        background-color: #4CAF50;
+        color: white;
+        cursor: pointer;
+        border: none;
+        ">Update</button>
 
     </form>
 </body>
--- login/update1.php
+++ login/update1.php
@@ -5,7 +5,8 @@ session_start();
 $id=$_SESSION['id'];  
 $username=$_POST['username'];
 $password=$_POST['password'];
-$sql="UPDATE `user` set `username`='$username',`password`='$password' WHERE `id`='$id'";
+$date=$_POST['date'];
+$sql="UPDATE `user` set `username`='$username',`password`='$password',`date`='$date' WHERE `id`='$id'";
 $res=$conn->query($sql);
 if($res){
     echo "<script> alert('Rows has been updated'); </script>";
